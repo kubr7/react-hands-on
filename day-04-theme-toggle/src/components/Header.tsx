eeef72e71f6e821f815dbb54f2c56a15b7071991
kubr <kubrtech@gmail.com>
2026-01-07 05:02:43 +0530

    feat: add theme toggle UI components

---
+++ day-04-theme-toggle/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import ThemeToggle from "./ThemeToggle";
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Theme Toggle App</h1>
+            <ThemeToggle />
+        </header>
+    );
+};
+
+export default Header;
