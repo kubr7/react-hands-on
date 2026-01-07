63188eb1fa67ad955f2305dcb8e82b9199d177f8
kubr <kubrtech@gmail.com>
2026-01-07 22:07:15 +0530

    refactor: generate password on user action instead of auto-derivation

--- day-05-password-generator/src/hooks/usePassword.ts
+++ day-05-password-generator/src/hooks/usePassword.ts
@@ -3,10 +3,10 @@ import { generatePassword } from "../utils/generatePassword";
 
 export function usePassword() {
     const [length, setLength] = useState(7);
-    const [uppercase, setUppercase] = useState(true);
+    const [uppercase, setUppercase] = useState(false);
     const [lowercase, setLowercase] = useState(false);
     const [numbers, setNumbers] = useState(false);
-    const [symbols, setSymbols] = useState(true);
+    const [symbols, setSymbols] = useState(false);
 
     const [password, setPassword] = useState("");
 
