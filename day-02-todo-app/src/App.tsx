36cbe0dfdbd6977a0a35fb692cf4b7c406d94272
kubr <kubrtech@gmail.com>
2026-01-11 05:50:31 +0530

    refactor (styles): improve UI design

--- day-02-todo-app/src/App.tsx
+++ day-02-todo-app/src/App.tsx
@@ -2,6 +2,8 @@ import { useTodos } from "./hooks/useTodos";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 import TodoFilter from "./components/TodoFilter";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./styles/todo.css";
 
 function App() {
@@ -15,15 +17,18 @@ function App() {
   } = useTodos();
 
   return (
-    <div className="todo-app">
-      <h1>Todo App</h1>
-      <TodoInput onAdd={addTodo} />
-      <TodoFilter filter={filter} setFilter={setFilter} />
-      <TodoList
-        todos={todos}
-        onToggle={toggleTodo}
-        onDelete={deleteTodo}
-      />
+    <div className="app-container">
+      <Header />
+      <div className="todo-app">
+        <TodoInput onAdd={addTodo} />
+        <TodoFilter filter={filter} setFilter={setFilter} />
+        <TodoList
+          todos={todos}
+          onToggle={toggleTodo}
+          onDelete={deleteTodo}
+        />
+      </div>
+      <Footer />
     </div>
   );
 }
